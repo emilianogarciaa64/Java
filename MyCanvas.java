39299e80178487c9a17543ebb92627593e88ddca
emilianogarciaa64 <2001329c@umich.mx>
2022-10-02 23:41:32 -0500

    Montañas y arboles

--- MyCanvas.java
+++ MyCanvas.java
@@ -12,10 +12,9 @@ public class MyCanvas extends JPanel {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setColor(new Color (127,233,245));
-        g.fillRect(0,0, 400, 100);
-
-        g.setColor(Color.YELLOW);
-        g.fillOval(40, 40, 20, 20);
+        Paintbrush paintBrush = new Paintbrush(g);
+        paintBrush.drawSky();
+        paintBrush.drawMountains();
+        paintBrush.drawTree();
     }
 }
